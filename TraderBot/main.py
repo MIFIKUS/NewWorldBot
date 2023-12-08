91365dd72b42743744856d87c0b1f9e2590f226b
LexaNeboxod <Seryi2144@gmail.com>
2023-12-08 23:57:33 +0300

    dev version 0.13

--- TraderBot/main.py
+++ TraderBot/main.py
@@ -4,7 +4,7 @@ from MainClasses.Image.image import Image
 from MainClasses.GoogleSheets.google_sheets import GoogleSheets
 
 import time
-
+import json
 
 google_sheets = GoogleSheets("https://docs.google.com/spreadsheets/d/1nFUIPW6nUTc5CfMroHsCCKzlOG4-9rEZGVmSHc9zMog/edit#gid=0",
                              "E:\\projects\\NewWorldBot\\services_files\\credentials.json")
@@ -13,9 +13,14 @@ mouse = Mouse()
 image = Image()
 
 
-categories = {{"Refined Wood"}}
+with open("E:\\projects\\NewWorldBot\\TraderBot\\Jsons\\catigories_cords.json", 'r', encoding='utf-8') as data:
+    categories = json.load(data)
+
+main_categories = categories.keys()
 cord = (615, 315, 1780, 400)
 AMOUNT_OF_PRODUCTS = 9
+cell_num = 2
+end_sell = 0
 
 
 def start():
@@ -23,17 +28,30 @@ def start():
 
 
 def search_products(hwnd):
+    time.sleep(1)
     mouse.move_and_click(125, 600)
+    time.sleep(1)
     mouse.move_and_click(300, 640)
-    mouse.move_and_click(250, 435)
-    parse_products()
+
+    for i in main_categories:
+        category_dict = categories.get(i)
+        category_cords = category_dict.get("cords")
+
+        mouse.move_and_click(category_cords[0], category_cords[1])
+
+        sub_categories = category_dict.get("categories")
+
+        for sub_category_name, sub_category_cords in sub_categories.items():
+            time.sleep(1)
+            mouse.move_and_click(sub_category_cords[0], sub_category_cords[1])
+            parse_products(i, sub_category_name)
 
 
-def parse_products():
+def parse_products(main_name, name):
     num = 0
     mouse.move(1800, 288)
     for a in range(3):
-        time.sleep(2)
+        time.sleep(3)
         if a < 2:
 
             for i in range(AMOUNT_OF_PRODUCTS):
@@ -53,19 +71,41 @@ def parse_products():
                                       (985, 900 + 77 * b, 1145, 985 + 77 * b))
                 image.take_screenshot(f"E:\\projects\\NewWorldBot\\TraderBot\\images\\screenshots\\avail{num}.png",
                                       (1690, 900 + 77 * b, 1790, 985 + 77 * b))
-    write_in_sheets()
+
+    mouse.move_and_click(260, 310)
+    write_in_sheets(main_name, name)
 
 
-def write_in_sheets():
-
+def write_in_sheets(main_name, name):
+    global cell_num
+    global end_sell
+    price_list = []
+    avail_list = []
+    main_name_list = []
+    name_list = []
     for i in range(1, 21):
-        cell_num = i + 1
-        price = image.image_to_string(f"E:\\projects\\NewWorldBot\\TraderBot\\images\\screenshots\\price{i}.png", True)
-        avail = image.image_to_string(f"E:\\projects\\NewWorldBot\\TraderBot\\images\\screenshots\\avail{i}.png", True)
-        google_sheets.write_google({f"C{cell_num}": price})
-        google_sheets.write_google({f"D{cell_num}": avail})
+
+        image.delete_all_colors_except_one(f"E:\\projects\\NewWorldBot\\TraderBot\\images\\screenshots\\price{i}.png",
+                                           [106, 96, 79], [176, 166, 150])
+        image.delete_all_colors_except_one(f"E:\\projects\\NewWorldBot\\TraderBot\\images\\screenshots\\avail{i}.png",
+                                           [106, 96, 79], [176, 166, 150])
+        price = image.image_to_string(f"E:\\projects\\NewWorldBot\\TraderBot\\images\\screenshots\\price{i}.png",
+                                      True)
+        avail = image.image_to_string(f"E:\\projects\\NewWorldBot\\TraderBot\\images\\screenshots\\avail{i}.png",
+                                      True)
+        price_list.append([price])
+        avail_list.append([avail])
+        main_name_list.append([main_name])
+        name_list.append([name])
+        end_sell += 1
+
+    google_sheets.write_google({f"A{cell_num}:A{end_sell+1}": main_name_list})
+    google_sheets.write_google({f"B{cell_num}:B{end_sell+1}": name_list})
+    google_sheets.write_google({f"C{cell_num}:C{end_sell+1}": price_list})
+    google_sheets.write_google({f"D{cell_num}:D{end_sell+1}": avail_list})
+    cell_num = end_sell + 1
 
 
-
-write_in_sheets()
-#start()
+#write_in_sheets("Refined woods", "Timber")
+start()
+#search_products(None)
